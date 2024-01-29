0474bc6f2ca4b8ec9ef57886e86c481cd0e849c1
Shivam <shivamlather5@gmail.com>
2024-01-30 01:17:48 +0530

    Added email to usermodel

--- Backend/models/usersmodel.js
+++ Backend/models/usersmodel.js
@@ -16,6 +16,10 @@ const userSchema = mongoose.Schema({
     profilepicture:{
         type:String,
         default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+    },
+    email:{
+        type:String,
+        required:true,
     }
     },{
         timestamps:true
