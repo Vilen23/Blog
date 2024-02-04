4a89de51078a9f1b906f3906aa7391f9d96e57ae
Shivam <Shivamlather5@gmail.com>
2024-02-04 19:32:52 +0530

    Fixed getPost

--- Backend/controllers/post.controller.js
+++ Backend/controllers/post.controller.js
@@ -2,28 +2,74 @@ const { Post } = require("../models/post.model");
 const { errorHandler } = require("../utils/error");
 
 const create = async (req, res, next) => {
-    console.log(req.user);
+  console.log(req.user);
   if (!req.user.isAdmin) {
     return next(errorHandler(401, "You are not admin"));
   }
-  if(!req.body.title || !req.body.content){
-    return next(errorHandler(400,"All fields are required"))
+  if (!req.body.title || !req.body.content) {
+    return next(errorHandler(400, "All fields are required"));
   }
   const slug = req.body.title.split(" ").join("-").toLowerCase();
   const newPost = new Post({
     ...req.body,
     slug,
-    userId:req.user.id
+    userId: req.user.id,
   });
-  try{
+  try {
     const savedPost = await newPost.save();
     res.status(200).json(savedPost);
+  } catch (error) {
+    next(error);
   }
-    catch(error){
-        next(error);
-    }
+};
+
+
+ const getPosts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === 'asc' ? 1 : -1;
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: 'i' } },
+          { content: { $regex: req.query.searchTerm, $options: 'i' } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalPosts = await Post.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      posts,
+      totalPosts,
+      lastMonthPosts,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
   create,
+  getPosts,
 };
--- Backend/routes/post.route.js
+++ Backend/routes/post.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { create } = require('../controllers/post.controller');
+const { create, getPosts } = require('../controllers/post.controller');
 const { verifyToken } = require('../utils/VerifyUser');
 
 router.post("/create", verifyToken, create);
+router.get("/getposts",getPosts);
 
 module.exports = router;
