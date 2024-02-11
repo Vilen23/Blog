812d3b02752b6a24dd64e1c9b7631b750fbfdbbb
Shivam <Shivamlather5@gmail.com>
2024-02-11 16:46:41 +0530

    Added Home page

--- Frontend/src/comp/PostCard.jsx
+++ Frontend/src/comp/PostCard.jsx
@@ -1,26 +1,30 @@
 import { useNavigate } from "react-router-dom";
 
 export function PostCard({ post }) {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
-    <div className=" border-teal-500 hover:border-2 transition-all duration-200  group flex flex-col relative w-full h-[380px] border overflow-hidden rounded-lg sm:w-[460px]">
-      <img
-        src={post.image}
-        alt=""
-        className="cursor-pointer h-[260px] w-[500px] self-center object-cover group-hover:h-[200px] transition-all duration-300 z-20"
-      onClick={()=>{
-        navigate(`/post/${post.slug}`)
-      }}/>
-      <div className="p-3 flex flex-col gap-2">
-        <p className="text-lg font-bold line-clamp-2 text-center">{post.title}</p>
-        <span className="text-sm text-gray-500 italic text-center">{post.category}</span>
-        <p className="z-10 group-hover:bottom-0 absolute bottom-[-200px] opacity-0 group-hover:opacity-100 border-teal-500 left-0 right-0 text-teal-500 hover:bg-teal-500 hover:text-white transition-all border-[1px]  duration-300 text-center py-2 m-2 cursor-pointer  !rounded-tl-none rounded-md "
+    <div className='group relative border border-teal-500 hover:border-2 h-[340px] w-[450px] overflow-hidden rounded-lg transition-all'>
+      
+        <img
+          src={post.image}
+          alt='post cover'
+          className='h-[220px] w-full object-cover group-hover:h-[200px] transition-all duration-300'
         onClick={()=>{
-            navigate(`/post/${post.slug}`)
-          }}>
-          Read More
-        </p>
+          navigate(`/post/${post.slug}`)
+        }}/>
+      
+      <div className='p-3 flex flex-col gap-2 h-[140px]'> 
+        <p className='text-lg text-gray-700 font-bold line-clamp-1 text-center'>{post.title}</p>
+        <span className='italic text-sm text-center'>{post.category}</span>
       </div>
+      <p
+        onClick={()=>{
+          navigate(`/post/${post.slug}`)
+        }}
+        className='cursor-pointer z-10 group-hover:bottom-0 absolute bottom-[-200px] left-0 right-0 border border-teal-500 text-teal-500 hover:bg-teal-500 hover:text-white transition-all duration-300 text-center py-2 rounded-md !rounded-tl-none m-2'
+      >
+        Read article
+      </p>
     </div>
   );
 }
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,5 +1,69 @@
-export function Home(){
-    return <div>
-        
+import { useEffect, useState } from "react";
+import { CalltoAction } from "../comp/CalltoAction";
+import { PostCard } from "../comp/PostCard";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+export function Home() {
+  const [posts, setPosts] = useState([]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    try {
+      const fetchPosts = async () => {
+        const res = await axios.get(
+          "http://localhost:3000/api/post/getposts"
+        );
+        if (res.status === 200) {
+          setPosts(res.data.posts);
+        }
+      };
+      fetchPosts();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+  console.log(posts);
+  return (
+    <div>
+      <div className="flex flex-col gap-6 px-3 p-28 max-w-6xl mx-auto">
+        <h1 className="text-3xl font-bold lg:text-6xl">Welcome to my Blog</h1>
+        <p className="text-gray-500 text-xs sm:text-sm">
+          Welcome to our cutting-edge blogging platform, where the power of the
+          MERN stack (MongoDB, Express.js, React, and Node.js) meets the finesse
+          of Recoil for state management, delivering a seamless and dynamic user
+          experience. Our commitment to security and personalization is evident
+          through the use of cookies for authentication, ensuring that your
+          session is both secure and tailored to your preferences. Furthermore,
+          we've integrated advanced persistence features to guarantee that your
+          interactions, from reading articles to writing posts, are smoothly
+          preserved across visits. This fusion of technologies ensures that our
+          platform is not only a beacon of modern web development practices but
+          also a haven for users seeking a reliable, efficient, and personalized
+          blogging experience.
+        </p>
+        <p className="text-xs sm:text-sm text-cyan-500 font-bold hover:underline cursor-pointer">
+          View all Posts
+        </p>
+      </div>
+      <div className="p-5 bg-slate-100">
+        <CalltoAction />
+      </div>
+      <div className="w-full mx-auto flex flex-col py-7 p-3 gap-8 ">
+        {posts && posts.length > 0 && (
+          <div className="">
+            <h2 className="text-2xl font-bold text-center my-10">Recent Posts</h2>
+            <div className="flex flex-wrap gap-4 justify-center ">
+              {posts &&
+                posts.map((post) => {
+                  return <PostCard key={post._id} post={post} />;
+                })}
+            </div>
+          </div>
+        )}
+      <p className="text-cyan-500 text-center cursor-pointer hover:underline text-bold" onClick={()=>{
+        navigate('/search')
+      }}>View All Posts</p>
+      </div>
     </div>
+  );
 }
